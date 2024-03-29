f6d165c4aaab02386ab7648d38c66186215cd23a
pascalecodes <pdelauna@gmail.com>
2024-03-28 20:36:29 -0700

    profile page UI add profile content

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -47,7 +47,7 @@ function SignUp() {
      <input type='text' placeholder='username' className='border p-3 rounded-lg' id='username' onChange={handleChange}/>
      <input type='email' placeholder='email' className='border p-3 rounded-lg' id='email' onChange={handleChange}/>
      <input type='password' placeholder='password' className='border p-3 rounded-lg' id='password' onChange={handleChange}/>
-     <input type='text' placeholder='FirstName' className='border p-3 rounded-lg' id='firstName' onChange={handleChange}/>
+     <input type='text' placeholder='firstName' className='border p-3 rounded-lg' id='firstName' onChange={handleChange}/>
      <button disabled={loading} className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80'> {loading ? 'Loading...' : 'Sign Up'}</button>
      <OAuth />
      </form>
