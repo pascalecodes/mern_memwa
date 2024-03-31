ab2b57f56e378b4ff3f5ecd9c9636723c2d4cd02
pascalecodes <pdelauna@gmail.com>
2024-03-30 18:52:33 -0700

    create update user api route

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRouter from './routes/user.route.js'
 import authRouter from './routes/auth.route.js'
+import cookieParser from 'cookie-parser';
 
 dotenv.config()
 
@@ -17,6 +18,8 @@ mongoose.connect(process.env.MONGO).then(() => {
 const app = express()
 
 app.use(express.json())
+
+app.use(cookieParser)
 
 app.listen(7100, ()=> {
     console.log('Server is running on port 7100')
