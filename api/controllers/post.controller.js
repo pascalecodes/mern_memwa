57b6f652126ce5102ff0fc2aea0c144f6244abb8
pascalecodes <pdelauna@gmail.com>
2024-04-08 22:09:07 -0700

    create update post api route

--- api/controllers/post.controller.js
+++ api/controllers/post.controller.js
@@ -1,5 +1,6 @@
 import Post from "../models/post.model.js";
-import { errorHandler } from '../utils/error.js';
+import { errorHandler } from "../utils/error.js";
+
 
 export const createPost = async (req, res, next) => {
   try {
@@ -24,6 +25,27 @@ export const deletePost = async (req, res, next) => {
     await Post.findByIdAndDelete(req.params.id);
     res.status(200).json('Listing has been deleted!');
   } catch (error){
+    next(error);
+  }
+};
+
+export const updatePost = async (req, res, next) => {
+  const post = await Post.findById(req.params.id);
+  if (!post) {
+    return next(errorHandler(404, 'Post not found!'));
+  }
+  if (req.user.id !== post.userRef) {
+    return next(errorHandler(401, 'You can only update your own posts!'));
+  }
+
+  try {
+    const updatedPost = await Post.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      { new: true }
+    );
+    res.status(200).json(updatedPost);
+  } catch (error) {
     next(error);
   }
 };
--- api/routes/post.route.js
+++ api/routes/post.route.js
@@ -1,10 +1,11 @@
 import express from 'express'
 import { verifyToken } from '../utils/verifyUser.js';
-import { createPost, deletePost } from '../controllers/post.controller.js';
+import { createPost, deletePost, updatePost } from '../controllers/post.controller.js';
 
 const router = express.Router();
 
 router.post('/create', verifyToken, createPost);
 router.delete('/delete/:id', verifyToken, deletePost);
+router.post('/update/:id', verifyToken, updatePost);
 
 export default router
