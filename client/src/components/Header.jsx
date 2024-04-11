e1a3789034661b5eb8cc4549848fa81014b9ac3c
pascalecodes <pdelauna@gmail.com>
2024-04-10 21:37:38 -0700

    complete header search form functionality

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,9 +1,29 @@
 import {FaSearch} from 'react-icons/fa'
-import {Link} from 'react-router-dom'
+import {Link, useNavigate} from 'react-router-dom'
 import { useSelector } from 'react-redux'
+import { useEffect, useState } from 'react'
 
-function Header() {
+export default function Header () {
   const {currentUser} = useSelector(state => state.user)
+  const [searchTerm, setSearchTerm] = useState('');
+  const navigate = useNavigate();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const urlParams = new URLSearchParams(window.location.search);
+    urlParams.set('searchTerm', searchTerm);
+    const searchQuery = urlParams.toString();
+    navigate(`/search?${searchQuery}`);
+  };
+
+  useEffect(() => {
+    const urlParams = new URLSearchParams(location.search);
+    const searchTermFromUrl = urlParams.get('searchTerm');
+    if (searchTermFromUrl) {
+      setSearchTerm(searchTermFromUrl);
+    }
+  }, [location.search]);
+
   return (
     <header className='bg-slate-200 shadow-md'>
       <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
@@ -19,9 +39,17 @@ function Header() {
         <Link to='/watch'><li className='hidden sm:inline text-slate-700 hover:underline'>Watch</li></Link>
         <Link to='/find'><li className='hidden sm:inline text-slate-700 hover:underline'>Find</li></Link>
       </ul>
-     <form className='bg-slate-100 p-3 rounded-lg flex items-center'  action="/find" method="GET">
-      <input type="search" placeholder="Search...." aria-label="Search" name="searchTerm" className='bg-transparent focus:outline-none w-24 sm:w-64'/>
-     <FaSearch className='text-slate-600' />
+     <form onSubmit={handleSubmit} className='bg-slate-100 p-3 rounded-lg flex items-center'>
+      <input type="search" placeholder="Search...." aria-label="Search" name="searchTerm" className='bg-transparent focus:outline-none w-24 sm:w-64'
+      value={searchTerm}
+      onChange={(e) => setSearchTerm(e.target.value)}
+      
+      />
+      <button>
+
+      <FaSearch className='text-slate-600' />
+      </button>
+     
      </form>
      <ul className='flex gap-2'>
       <Link to='/profile'>
@@ -34,7 +62,6 @@ function Header() {
     </div>
     
     </header>
-  )
+  );
 }
 
-export default Header
