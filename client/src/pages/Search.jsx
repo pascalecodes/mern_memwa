551ac6b63b2cd8261d3515312f11149ea7acfd5e
pascalecodes <pdelauna@gmail.com>
2024-04-11 20:59:14 -0700

    add handleChange effect for search filters

--- client/src/pages/Search.jsx
+++ client/src/pages/Search.jsx
@@ -1,63 +1,131 @@
-import React from 'react'
+import { useState } from 'react'
+import { useNavigate } from 'react-router-dom';
 
 export default function Search() {
-  return (
+    const navigate = useNavigate();
+    const [sidebardata, setSidebardata] = useState({
+        searchTerm: '',
+        // include: 'all',
+        sort: 'created_at',
+        order: 'desc',
+    });
+
+const handleChange = (e) =>{
+    // if(e.target.id === 'all'|| e.target.id === 'title' || e.target.id === 'description'|| e.target.id === 'tags'|| e.target.id === 'author') {
+    //     setSidebardata({...sidebardata, include: e.target.id})
+    // }
+    
+    if (e.target.id === 'searchTerm') {
+        setSidebardata({ ...sidebardata, searchTerm: e.target.value });
+      }
+
+    // if(e.target.id === 'video' || 'pictures' || 'other'){
+    //     setSidebardata({...sidebardata, [e.target.id]: e.target.checked || e.target.checked === 'true' ? true : false, });
+    // }
+    
+    if (e.target.id === 'sort_order') {
+        const sort = e.target.value.split('_')[0] || 'created_at';
+  
+        const order = e.target.value.split('_')[1] || 'desc';
+  
+        setSidebardata({ ...sidebardata, sort, order });
+      }
+
+};
+
+const handleSubmit = (e) => {
+    e.preventDefault();
+    const urlParams = new URLSearchParams();
+    urlParams.set('searchTerm', sidebardata.searchTerm);
+    urlParams.set('sort', sidebardata.sort);
+    urlParams.set('order', sidebardata.order);
+    const searchQuery = urlParams.toString();
+    navigate(`/search?${searchQuery}`);
+  };
+
+return (
     <div className='flex flex-col md:flex-row'>
         <div className='p-7 border-b-2 md:border-r-2 md:min-h-screen'>
-        <form className='flex flex-col gap-8'> 
+        <form onSubmit={handleSubmit} className='flex flex-col gap-8'>
             <div className='flex items-center gap-2'>
                 <label className='whitespace-nowrap font-semibold'>Search Term:</label>
-                <input type='text' id='searchTerm' placeholder='Search...'
-                className='border rounded=lg p-3 w-full'/>
+                 <input
+                type='text'
+                id='searchTerm'
+                placeholder='Search...'
+                className='border rounded-lg p-3 w-full'
+                value={sidebardata.searchTerm}
+                onChange={handleChange}
+                />
             </div>
             {/* these are not functional yet */}
-            <div className='flex gap-2 flex-wrap items-center'>
+            {/* <div className='flex gap-2 flex-wrap items-center'>
                 <label className='font-semibold'>Include:</label>
                 <div className='flex gap-2'>
-                    <input type='checkbox' id='all' className='w-5'/>
+                    <input type='checkbox' id='all' className='w-5'
+                    onChange={handleChange}
+                    checked={sidebardata.include ==='all'}
+                    />
                     <span>All</span>
                 </div>
                 <div className='flex gap-2'>
-                    <input type='checkbox' id='title' className='w-5'/>
+                    <input type='checkbox' id='title' className='w-5'
+                    onChange={handleChange}
+                    checked={sidebardata.title}/>
                     <span>Title</span>
                 </div>
                 <div className='flex gap-2'>
-                    <input type='checkbox' id='description' className='w-5'/>
+                    <input type='checkbox' id='description' className='w-5'
+                    onChange={handleChange}
+                    checked={sidebardata.description}/>
                     <span>Description</span>
                 </div>
                 <div className='flex gap-2'>
-                    <input type='checkbox' id='tags' className='w-5'/>
+                    <input type='checkbox' id='tags' className='w-5'
+                    onChange={handleChange}
+                    checked={sidebardata.tags}/>
                     <span>Tags</span>
                 </div>
                 <div className='flex gap-2'>
-                    <input type='checkbox' id='author' className='w-5'/>
+                    <input type='checkbox' id='author' className='w-5'
+                    onChange={handleChange}
+                    checked={sidebardata.author}/>
                     <span>Author</span>
                 </div>
             </div>
             <div className='flex gap-2 flex-wrap items-center'>
                 <label className='font-semibold'>Type:</label>
                 <div className='flex gap-2'>
-                    <input type='checkbox' id='video' className='w-5'/>
+                    <input type='checkbox' id='video' className='w-5'
+                    onChange={handleChange}
+                    checked={sidebardata.video}/>
                     <span>Video</span>
                 </div>
                 <div className='flex gap-2'>
-                    <input type='checkbox' id='pictures' className='w-5'/>
+                    <input type='checkbox' id='pictures' className='w-5'
+                    onChange={handleChange}
+                    checked={sidebardata.pictures}/>
                     <span>Pictures</span>
                 </div>
                 <div className='flex gap-2'>
-                    <input type='checkbox' id='other' className='w-5'/>
+                    <input type='checkbox' id='other' className='w-5'
+                    onChange={handleChange}
+                    checked={sidebardata.other}/>
                     <span>Other</span>
                 </div>
-            </div>
+            </div> */}
             {/* above need to be configured */}
-            <div>
-                <label className='font-semibold'>Sort:</label>
-                <select id="sort_order"
-                className='border rounded-lg p-3'>
-                    <option>Likes High to Low</option>
-                    <option>Likes Low to High</option>
-                    <option>Latest</option>
-                    <option>Oldest</option>
+            <div className='flex items-center gap-2'>
+                <label className='font-semibold'>Sort:  </label>
+                <select 
+                    onChange={handleChange}
+                    defaultValue={'created_at_desc'}
+                    id='sort_order'
+                    className='border rounded-lg p-3'>
+                    {/* <option value='likes_desc'>Likes High to Low</option>
+                    <option value='likes_asc'>Likes Low to High</option> */}
+                    <option value='createdAt_desc'>Latest</option>
+                    <option value='createdAt_asc'>Oldest</option>
                 </select>
             </div>
             <button className='bg-slate-700 text-white p-3 rounded-lg uppercase hove:opacity-95'>Search</button>
@@ -69,5 +137,5 @@ export default function Search() {
 
         </div>
     </div>
-  )
+  ) 
 }
