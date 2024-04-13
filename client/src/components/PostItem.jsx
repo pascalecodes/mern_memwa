3c101867ed11ae3afc73c4f0de634beb0285afa3
pascalecodes <pdelauna@gmail.com>
2024-04-12 21:39:57 -0700

    styling edit for card

--- client/src/components/PostItem.jsx
+++ client/src/components/PostItem.jsx
@@ -24,7 +24,7 @@ export default function PostItem({post}) {
         <img 
         src={post.mediaUrls[0] || "https://i.pinimg.com/originals/4e/08/f5/4e08f596f8fc6eb3c1eee8295b40bae4.jpg"} 
         alt='post cover' 
-        className="h-[300px] sm:h-[220px] w-full object-cover hover:scale-105 transition-scale duration-300"/>
+        className='h-[320px] sm:h-[220px] w-full object-cover hover:scale-105 transition-scale duration-300'/>
         <div className="p-3 flex flex-col gap-2 w-full">
             <p className="truncate text-lg font-semibold text-slate-700 ">{post.title}</p>
            
