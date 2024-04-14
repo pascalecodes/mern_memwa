69ba93e0b7ad6bbe3d7788b58721528f4b0dbc56
pascalecodes <pdelauna@gmail.com>
2024-04-13 20:42:16 -0700

    add pre-installation config for render upload

--- api/index.js
+++ api/index.js
@@ -6,22 +6,23 @@ import userRouter from './routes/user.route.js';
 import authRouter from './routes/auth.route.js';
 import postRouter from './routes/post.route.js';
 import cookieParser from 'cookie-parser';
+import path from 'path';
 
 dotenv.config()
-
 
 mongoose.connect(process.env.MONGO).then(() => {
     console.log('Connected to MongoDB!');
 }).catch((err) => { 
     console.log(err)
-})
+});
 
+const __dirname = path.resolve();
 
-const app = express()
+const app = express();
 
-app.use(express.json())
+app.use(express.json());
 
-app.use(cookieParser())
+app.use(cookieParser());
 
 app.listen(7100, ()=> {
     console.log('Server is running on port 7100')
@@ -30,6 +31,12 @@ app.listen(7100, ()=> {
 app.use('/api/user', userRouter);
 app.use('/api/auth', authRouter);
 app.use('/api/post', postRouter);
+
+app.use(express.static(path.join(__dirname, '/client/dist')));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+  })
 
 app.use((err, req, res, next) => {
    const statusCode = err.statusCode || 500; 
--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -27,34 +27,40 @@ export default function Header () {
   return (
     <header className='bg-slate-200 shadow-md'>
       <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
+        <div className='flex-auto '>
         <Link to='/'>
         <h1 className='font-bold text-sm sm:text-xl flex flex-wrap'>
               <img src="/img/logo.svg"  alt="logo"/>
           <span className=" text-blue-500" >MEMWA</span>
         </h1>
         </Link>
-      <ul className='flex gap-4'>
+        </div>
+
+        <div >
+      <ul className='flex flex-auto gap-8 pr-8 '>
         <Link to='/'><li className='hidden sm:inline text-slate-700 hover:underline'>Home</li> </Link>
         <Link to='/capture'><li className='hidden sm:inline text-slate-700 hover:underline'>Capture</li></Link>
         <Link to='/watch'><li className='hidden sm:inline text-slate-700 hover:underline'>Watch</li></Link>
         <Link to='/search'><li className='hidden sm:inline text-slate-700 hover:underline'>Find</li></Link>
       </ul>
-    
-    
+      </div>
 
+    <div className='flex'>
      <form onSubmit={handleSubmit} className='bg-slate-100 p-3 rounded-lg flex items-center'>
       <input type="search" placeholder="Search...." aria-label="Search" name="searchTerm" className='bg-transparent focus:outline-none w-24 sm:w-64'
       value={searchTerm}
       onChange={(e) => setSearchTerm(e.target.value)}
-      
       />
       <button>
 
       <FaSearch className='text-slate-600' />
       </button>
      </form>
+     </div>
 
-     <ul className='flex gap-2'>
+     <div className='flex pl-3'>
+
+     <ul>
       <Link to='/profile'>
         {currentUser ? (
          <li className='hidden sm:inline text-slate-700 hover:underline'><img className='rounded-full h-7 w-7 object-cover' src={currentUser.avatar} alt='profile' /> Profile</li> 
@@ -62,6 +68,7 @@ export default function Header () {
         </Link>
       {/* <Link to='/sign-up'><li className='hidden sm:inline text-slate-700 hover:underline'>Sign Up</li></Link> */}
      </ul>
+     </div>
     </div>
     
     </header>
