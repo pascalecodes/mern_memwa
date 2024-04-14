109c4e16929cd4b2456d01d0d155129077f5d022
pascalecodes <pdelauna@gmail.com>
2024-04-13 21:06:28 -0700

    edit header styling

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -27,25 +27,25 @@ export default function Header () {
   return (
     <header className='bg-slate-200 shadow-md'>
       <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
-        <div className='flex-auto '>
+       
         <Link to='/'>
         <h1 className='font-bold text-sm sm:text-xl flex flex-wrap'>
               <img src="/img/logo.svg"  alt="logo"/>
           <span className=" text-blue-500" >MEMWA</span>
         </h1>
         </Link>
-        </div>
+     
 
-        <div >
+     
       <ul className='flex flex-auto gap-8 pr-8 '>
         <Link to='/'><li className='hidden sm:inline text-slate-700 hover:underline'>Home</li> </Link>
         <Link to='/capture'><li className='hidden sm:inline text-slate-700 hover:underline'>Capture</li></Link>
         <Link to='/watch'><li className='hidden sm:inline text-slate-700 hover:underline'>Watch</li></Link>
         <Link to='/search'><li className='hidden sm:inline text-slate-700 hover:underline'>Find</li></Link>
       </ul>
-      </div>
+    
 
-    <div className='flex'>
+
      <form onSubmit={handleSubmit} className='bg-slate-100 p-3 rounded-lg flex items-center'>
       <input type="search" placeholder="Search...." aria-label="Search" name="searchTerm" className='bg-transparent focus:outline-none w-24 sm:w-64'
       value={searchTerm}
@@ -56,9 +56,7 @@ export default function Header () {
       <FaSearch className='text-slate-600' />
       </button>
      </form>
-     </div>
-
-     <div className='flex pl-3'>
+    
 
      <ul>
       <Link to='/profile'>
@@ -68,7 +66,7 @@ export default function Header () {
         </Link>
       {/* <Link to='/sign-up'><li className='hidden sm:inline text-slate-700 hover:underline'>Sign Up</li></Link> */}
      </ul>
-     </div>
+     
     </div>
     
     </header>
