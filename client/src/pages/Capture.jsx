74d28df5f8c5cd82e8887dfb2d644227f61a3800
pascalecodes <pdelauna@gmail.com>
2024-04-21 18:01:52 -0700

    change capture page

--- client/src/pages/Capture.jsx
+++ client/src/pages/Capture.jsx
@@ -1,12 +1,47 @@
-import React from 'react'
+import { useState, useRef, useEffect } from 'react';
+//import styles from './CapturePage.module.css'; // Import Tailwind styles
+
 
 function Capture() {
-  return (
-    // <div>
-    <div className='py-20 px-8 max-w-6xl mx-auto min-h-[20vh]'>
-      Capture
-    </div>
-  )
+  const [permission, setPermission] = useState(false);
+    const [stream, setStream] = useState(null);
+
+    const getCameraPermission = async () => {
+        if ("MediaRecorder" in window) {
+            try {
+                const streamData = await navigator.mediaDevices.getUserMedia({
+                    audio: true,
+                    video: true,
+                });
+                setPermission(true);
+                setStream(streamData);
+            } catch (err) {
+                alert(err.message);
+            }
+        } else {
+            alert("The MediaRecorder API is not supported in your browser.");
+        }
+    };
+
+    return (
+        <div>
+            <h2>Video Recorder</h2>
+            <main>
+                <div className="video-controls">
+                    {!permission ? (
+                        <button onClick={getCameraPermission} type="button">
+                            Get Camera
+                        </button>
+                    ):null}
+                    {permission ? (
+                        <button type="button">
+                            Record
+                        </button>
+                    ):null}
+                </div>
+            </main>
+        </div>
+    );
 }
 
 export default Capture
