6b455b45a0c626e282dc4dd2ca4debedda795372
pascalecodes <pdelauna@gmail.com>
2024-06-05 23:16:58 -0700

    fix recording audio and self view bugs

--- client/src/components/AnswerQuestion.jsx
+++ client/src/components/AnswerQuestion.jsx
@@ -7,15 +7,19 @@ const AnswerQuestion = () => {
   const [recordedVideo, setRecordedVideo] = useState(null);
   const mediaRecorderRef = useRef(null); // useRef hook to store MediaRecorder instance
   const liveVideoFeed= useRef(null);
+  const [recordingStatus, setRecordingStatus] = useState('');
 
   const startRecording = async () => {
     setIsRecording(true);
+    setRecordingStatus('Recording in progress...');
     try {
-      const mediaStream = await navigator.mediaDevices.getUserMedia({ video: true });
+      const mediaStream = await navigator.mediaDevices.getUserMedia({ video: true, audio: true });
+      
       const mediaRecorder = new MediaRecorder(mediaStream);
       mediaRecorder.ondataavailable = (e) => setRecordedVideo(e.data);
       mediaRecorderRef.current = mediaRecorder; // Store reference in useRef
       mediaRecorder.start();
+      liveVideoFeed.current.srcObject = mediaStream;
     } catch (error) {
       console.error('Error capturing video:', error);
       setIsRecording(false);
@@ -23,9 +27,14 @@ const AnswerQuestion = () => {
   };
 
   const stopRecording = () => {
-    setIsRecording(false);
+   
     if (mediaRecorderRef.current) {
-      mediaRecorderRef.current.stop(); // Use reference to stop recording
+      const recordedBlob = new Blob(mediaRecorderRef.current.stop(), {type: 'video/webm'}); // Use reference to stop recording
+    //   const recordedBlob = new Blob(mediaRecorderRef.current.chunks, { type: 'video/webm' });
+ 
+    setRecordedVideo(recordedBlob);
+    setIsRecording(false);
+    setRecordingStatus('');
     }
   };
 
@@ -33,16 +42,15 @@ const AnswerQuestion = () => {
     
     <div >
         <video ref={liveVideoFeed} autoPlay muted className='overlay'/> 
-        <button className='text-white text-center bg-green-700 p-2 hover:opacity-80 mr-3' onClick={startRecording} disabled={isRecording}>
-        {/* {isRecording ? 'Stop Recording' : 'Record Answer'} */} Start Recording
+        <div>{recordingStatus}</div>
+        <button className='text-white text-center bg-green-700 p-2 hover:opacity-80 mr-3' onClick={startRecording} disabled={isRecording}> Start
       </button>
-      <button className='text-white text-center bg-red-700 p-2 hover:opacity-80 ml-3' onClick={stopRecording} >
-        {/* {isRecording ? 'Stop Recording' : 'Record Answer'} */} Stop Recording
+      <button className='text-white text-center bg-red-700 p-2 hover:opacity-80 ml-3' onClick={stopRecording}> Stop
       </button>
 
       {recordedVideo && (
         <div>
-          <video src={URL.createObjectURL(recordedVideo)} controls />
+          <video src={URL.createObjectURL(recordedVideo)} controls type="video/webm"/>
           <button className='text-white text-center bg-red-700 p-2' onClick={() => setRecordedVideo(null)}>Re-record</button>
         </div>
       )}
