36a2da622e530c4673fd5f3bb5e17f0f368557fd
pascalecodes <pdelauna@gmail.com>
2024-07-09 22:06:56 -0700

    edit support page form

--- client/src/pages/Support.jsx
+++ client/src/pages/Support.jsx
@@ -19,15 +19,16 @@ export default function Support() {
       <h1 className='text-5xl font-bold mb-4 text-blue-600 text-center' >Memwa Support</h1>
       <div className="container px-4 py-5 my-5">
         <h2 className='text-2xl text-blue-700 mt-4'>Welcome to the Memwa Support website!</h2>
-        <p>This website is here to help you with using Memwa or with problems you may encounter.</p>
-        <p>You can find answers to frequently asked questions in our <span> </span> 
+        <p className='w-full'>This website is here to help you with using Memwa or with problems you may encounter.</p>
+        <p className='w-full'>You can find answers to frequently asked questions in our <span> </span> 
         <Link 
               to={`/faq`}
               className='text-blue-500 hover:opacity-95'>
                 FAQ
         </Link> section.</p>
         <p>If you can't find the answer to your question, you can contact us using the form below.</p>
-        <div className='flex'>
+        <div className='flex flex-col md:flex-row justify-between'>
+        <div className='md:w-1/2'>
         <form action="/hc/en-us/contact" method="post" className="form-row">
           <input className=" flex h-10 w-full mb-2" type="text" name="name" placeholder="Your name"/>
           <input className=" flex h-10 w-full  mb-2" type="email" name="email" placeholder="Your email address"/>
@@ -36,6 +37,7 @@ export default function Support() {
           {/* <input className=" flex w-full m-2" type="submit" value="Submit"/> */}
           <button className=' flex w-full mb-2 bg-slate-700 text-white p-1 rounded-lg uppercase hove:opacity-95' type="submit" value="Upload">Submit</button>
         </form>
+        </div>
         </div>
       
       <h2 className='text-2xl text-blue-700 pt-4 mt-4'>Tips for Creating a Great Post</h2>
