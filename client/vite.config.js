70229986c26aaa5ea3d64e1d61305f4f3e3dc2bc
pascalecodes <pdelauna@gmail.com>
2024-10-30 23:06:07 -0700

    add axios for api calls to vite config and client endpoint

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,8 +6,10 @@ export default defineConfig({
   server:{
     proxy:{
       '/api': {
-        target: 'http://localhost:7100',
+        target: 'http://localhost:7100', // backend server URL
         secure: false,
+        changeOrigin: true, // Changes the origin of the host header to the target URL
+        rewrite: (path) => path.replace(/^\/api/, ''), // Remove /api prefix for the backend
       },
     },
   },
