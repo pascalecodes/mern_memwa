f605d945cc6529c419d9bb54ca0277334bdc0423
pascalecodes <pdelauna@gmail.com>
2024-11-11 16:14:47 -0800

    add store for video reducer

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,9 +1,22 @@
 import { combineReducers, configureStore } from '@reduxjs/toolkit'
 import userReducer from './user/userSlice'
-import { persistReducer, persistStore } from 'redux-persist'
-import storage from 'redux-persist/lib/storage'
+//import { persistReducer, persistStore } from 'redux-persist'
+import videoReducer from "./videoSlice";
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+import storage from "redux-persist/lib/storage";
 
-const rootReducer = combineReducers({user: userReducer})
+
+
+const rootReducer = combineReducers({user: userReducer, video: videoReducer })
 
 const persistConfig ={
     key: 'root',
@@ -16,7 +29,10 @@ const persistedReducer = persistReducer(persistConfig, rootReducer)
 export const store = configureStore({
   reducer: persistedReducer,
   middleware: (getDefaultMiddleware) => getDefaultMiddleware({
-    serializableCheck: false,
+    // serializableCheck: false,
+    serializableCheck: {
+      ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+    },
   }),
 })
 
