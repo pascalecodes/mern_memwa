11c0a5ad19493dfb88b75f3d5fb8a4b86b172e1a
pascalecodes <pdelauna@gmail.com>
2024-11-16 21:56:12 -0800

    add new find by userRef route

--- api/routes/post.route.js
+++ api/routes/post.route.js
@@ -1,6 +1,6 @@
 import express from 'express'
 import { verifyToken } from '../utils/verifyUser.js';
-import { createPost, deletePost, updatePost, getPost, findPosts, getAllPosts, getByTag } from '../controllers/post.controller.js';
+import { createPost, deletePost, updatePost, getPost, findPosts, getAllPosts, getByTag, getPostsByUserRef } from '../controllers/post.controller.js';
 
 const router = express.Router();
 
@@ -11,5 +11,6 @@ router.get('/get/:id', getPost);
 router.get('/get', findPosts);
 router.get('/posts', getAllPosts)
 router.get('/tags', getByTag)
+router.get('/user/:userRef', getPostsByUserRef)
 
 export default router
