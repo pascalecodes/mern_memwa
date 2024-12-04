171984c4d46024fab37214d112e67a5b4b5f56e3
pascalecodes <pdelauna@gmail.com>
2024-12-03 23:18:08 -0800

    update get username to fix bug

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -8,6 +8,6 @@ router.get('/test', test )
 router.post('/update/:id', verifyToken, updateUser)
 router.delete('/delete/:id', verifyToken, deleteUser)
 router.get('/posts/:id', verifyToken, getUserPosts)
-router.get('/:id', verifyToken, getUser)
+router.get('/find/:id', getUser)
 
 export default router;
